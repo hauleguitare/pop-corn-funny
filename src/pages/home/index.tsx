9881d7d9484334097f0f451ff049dbeb2e6819b1
hauleguitare <alexander.dutyone@gmail.com>
2022-09-14 23:35:53 +0700

    Create Review Card

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -10,27 +10,6 @@ import { fetchGenres } from '@/api/fetchGenres';
 
 interface IHomePageProps {}
 
-interface InitialGenresContext {
-    [K: string]: IGenres[];
-}
-
-const initialGenresRequest = () => {
-    let init: InitialGenresContext = {
-        movie: [],
-        tv: [],
-    };
-    const movie = fetchGenres('movie');
-    const tv = fetchGenres('tv');
-    Promise.all([movie, tv])
-        .then((res) => {
-            init.movie = res[0].genres;
-            init.tv = res[1].genres;
-        })
-        .catch((err) => {
-            console.log('error: ', err);
-        });
-    return init;
-};
 const HomePage: React.FunctionComponent<IHomePageProps> = (props) => {
     return (
         <div className="container mt-4 max-w-[1280px]">
