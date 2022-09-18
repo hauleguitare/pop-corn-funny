0b8ad375f9c134a77d2fa31b502a1bd636d6ed80
hauleguitare <alexander.dutyone@gmail.com>
2022-09-18 16:52:14 +0700

    Create scroll to top

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -7,6 +7,7 @@ import * as React from 'react';
 import { BiMenuAltLeft } from 'react-icons/bi';
 import GroupMenu from './components/Menu/groupMenu';
 import ResponsiveMenuBar from './components/Responsive/menuBar';
+import { BsFillArrowUpCircleFill } from 'react-icons/bs';
 
 interface IHeaderProps {}
 
@@ -18,41 +19,56 @@ const Header: React.FunctionComponent<IHeaderProps> = (props) => {
         setOpenMenu(!openMenu);
     };
     return (
-        <header
-            className={`sticky top-0 left-0 right-0 z-10 ${
-                scrollPos > 0 ? 'up-tablet:bg-stone-darkest/30 opacity-80' : 'up-tablet:bg-stone-darkest opacity-100'
-            } bg-stone-darkest transition-all ease-linear duration-200 hover:bg-stone-darkest hover:opacity-100`}
-        >
-            <HeaderContext.Provider value={{ MenuList, LoginList }}>
-                <div className="container flex up-tablet:justify-between justify-start items-center h-16 max-w-7xl max-h-16">
-                    <div className="mx-2 up-tablet:hidden">
-                        <button onClick={handleOpenMenu}>
-                            <BiMenuAltLeft size={'40px'} className="fill-white" />
-                        </button>
-                    </div>
-                    <div className="flex flex-row items-center">
-                        <Logo title="POPCORN" className="text-2xl" />
-                        <GroupMenu />
-                    </div>
-                    <div className="hidden text-white up-tablet:flex flex-row font-oswald">
-                        <div className="px-2 py-2 relative">
-                            <SearchBar />
-                        </div>
-                        <div className="px-2 py-2 relative group">
-                            <a href="#">
-                                <img src={ProfileGuest} className="max-h-8 ring-2 rounded-full ring-white" />
-                            </a>
+        <>
+            <header
+                className={`sticky top-0 left-0 right-0 z-10 ${
+                    scrollPos > 0
+                        ? 'up-tablet:bg-stone-darkest/30 opacity-80'
+                        : 'up-tablet:bg-stone-darkest opacity-100'
+                } bg-stone-darkest transition-all ease-linear duration-200 hover:bg-stone-darkest hover:opacity-100`}
+            >
+                <HeaderContext.Provider value={{ MenuList, LoginList }}>
+                    <div className="container flex up-tablet:justify-between justify-start items-center h-16 max-w-7xl max-h-16">
+                        <div className="mx-2 up-tablet:hidden">
+                            <button onClick={handleOpenMenu}>
+                                <BiMenuAltLeft size={'40px'} className="fill-white" />
+                            </button>
                         </div>
                         <div className="flex flex-row items-center">
-                            <a href="#" className="px-2 py-2 relative group">
-                                <p>Login</p>
-                            </a>
+                            <Logo title="POPCORN" className="text-2xl" />
+                            <GroupMenu />
+                        </div>
+                        <div className="hidden text-white up-tablet:flex flex-row font-oswald">
+                            <div className="px-2 py-2 relative">
+                                <SearchBar />
+                            </div>
+                            <div className="px-2 py-2 relative group">
+                                <a href="#">
+                                    <img src={ProfileGuest} className="max-h-8 ring-2 rounded-full ring-white" />
+                                </a>
+                            </div>
+                            <div className="flex flex-row items-center">
+                                <a href="#" className="px-2 py-2 relative group">
+                                    <p>Login</p>
+                                </a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <ResponsiveMenuBar isOpenMenu={openMenu} handleOnMenu={handleOpenMenu} />
-            </HeaderContext.Provider>
-        </header>
+                    <ResponsiveMenuBar isOpenMenu={openMenu} handleOnMenu={handleOpenMenu} />
+                </HeaderContext.Provider>
+            </header>
+            <BsFillArrowUpCircleFill
+                onClick={() => {
+                    window.scrollTo({
+                        top: 0,
+                        behavior: 'smooth',
+                    });
+                }}
+                className={`${
+                    scrollPos > 500 ? 'visible opacity-50' : 'invisible opacity-0'
+                } hover:opacity-100 cursor-pointer transition-all ease-linear duration-150 fixed bottom-0 right-0 w-10 h-10 mx-4 my-4 up-mobile:mx-8 up-mobile:my-8 fill-white z-30`}
+            />
+        </>
     );
 };
 
